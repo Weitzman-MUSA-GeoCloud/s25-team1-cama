8ad4df198e454e2c4eb5b49e0cbcc3c4bb522998
BR <hbairun@gmail.com>
2025-03-27 18:19:38 -0400

    1.1 1.2 DONE

--- tasks/1_extract_opa_properties_data/week08-terminal-code.sh
+++ tasks/1_extract_opa_properties_data/week08-terminal-code.sh
@@ -14,7 +14,7 @@ pipreqs --ignore node_modules --force
 functions-framework --debug \
   --target extract_phl_opa_properties
 
-gcloud functions deploy extract_phl_opa_properties-attempt9 \
+gcloud functions deploy extract_phl_opa_properties-attempt10 \
 --region=us-east4 \
 --runtime=python312 \
 --source=. \
@@ -26,5 +26,18 @@ gcloud functions deploy extract_phl_opa_properties-attempt9 \
 --set-env-vars='DATA_LAKE_BUCKET=musa5090s25-team1-raw_data' \
 --trigger-http
 
-gcloud functions call extract_phl_opa_properties-attempt9 --region=us-east4 --gen2
+gcloud functions call extract_phl_opa_properties-attempt10 --region=us-east4 --gen2
 
+gcloud functions deploy prepare_phl_opa_properties-attempt2 \
+--region=us-east4 \
+--runtime=python312 \
+--source=. \
+--entry-point=prepare_phl_opa_properties \
+--service-account='data-pipeline-user@musa5090s25-team1.iam.gserviceaccount.com' \
+--timeout=999s \
+--memory=8Gi \
+--no-allow-unauthenticated \
+--set-env-vars='DATA_LAKE_BUCKET=musa5090s25-team1-raw_data,DESTINATION_DATA_LAKE_BUCKET=musa5090s25-team1-prepared_data' \
+--trigger-http
+
+gcloud functions call prepare_phl_opa_properties-attempt2 --region=us-east4 --gen2
