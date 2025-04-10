80c3e61ad11b83976e602eebaf1005162707280a
BR <hbairun@gmail.com>
2025-04-10 18:41:28 -0400

    7 works and is alive

--- tasks/7_tax_year_assessment_bins/issue7-terminal-code.sh
+++ tasks/7_tax_year_assessment_bins/issue7-terminal-code.sh
@@ -2,7 +2,7 @@ gcloud functions deploy create_tax_year_assessments \
 --region=us-east4 \
 --runtime=python312 \
 --source=. \
---entry-point=create_tax_year_assessments \
+--entry-point=run_sql \
 --service-account='data-pipeline-user@musa5090s25-team1.iam.gserviceaccount.com' \
 --timeout=999s \
 --memory=16Gi \
--- tasks/7_tax_year_assessment_bins/sql/create_derived_tax_year_assessment_bins.sql
+++ tasks/7_tax_year_assessment_bins/sql/create_derived_tax_year_assessment_bins.sql
@@ -4,6 +4,7 @@ CREATE OR REPLACE TABLE `musa5090s25-team1.derived.tax_year_assessment_bins` AS 
             assmnts.year AS tax_year,
             FLOOR(LOG(PARSE_NUMERIC(assmnts.market_value))) AS log_bins -- assessed values (market_value) were log transformed as to have a normal distribution on the chart to be made from this table
         FROM `musa5090s25-team1.source.opa_assessments` AS assmnts
+        WHERE SAFE_CAST(assmnts.market_value AS NUMERIC) > 0
     )
 
     SELECT
