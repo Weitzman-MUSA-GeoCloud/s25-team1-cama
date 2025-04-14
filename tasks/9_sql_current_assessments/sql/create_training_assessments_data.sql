f98621e556e8eda295ab9a500437cc6140120f30
BR <hbairun@gmail.com>
2025-04-14 14:04:19 -0400

    remove bundle sales AND include sale year

--- tasks/9_sql_current_assessments/sql/create_training_assessments_data.sql
+++ tasks/9_sql_current_assessments/sql/create_training_assessments_data.sql
@@ -1,17 +1,18 @@
 CREATE OR REPLACE TABLE `musa5090s25-team1.derived.current_assessments_model_training_data` AS (
     SELECT 
-        CAST(sale_price AS NUMERIC) AS sale_price,
-        IFNULL(NULLIF(basements, ""), "NA") AS basements,
-        IFNULL(NULLIF(building_code, ""), "NA") AS building_code,
-        IFNULL(NULLIF(census_tract, ""), "NA") AS census_tract,
-        IFNULL(NULLIF(exterior_condition, ""), "NA") AS exterior_condition,
-        IFNULL(NULLIF(zip_code, ""), "NA") AS zip_code,
-        IFNULL(NULLIF(zoning, ""), "NA") AS zoning,
-        IFNULL(NULLIF(number_of_bathrooms, ""), "NA") AS number_of_bathrooms,
-        IFNULL(NULLIF(number_of_bedrooms, ""), "NA") AS number_of_bedrooms,
-        IFNULL(NULLIF(number_stories, ""), "NA") AS number_stories,
-        IFNULL(NULLIF(total_area, ""), "NA") AS total_area,
-        IFNULL(NULLIF(year_built, ""), "NA") AS year_built
+        CAST(main.sale_price AS NUMERIC) AS sale_price,
+        SUBSTR(main.sale_date, 1, 4) AS sale_year,
+        IFNULL(NULLIF(main.basements, ""), "NA") AS basements,
+        IFNULL(NULLIF(main.building_code, ""), "NA") AS building_code,
+        IFNULL(NULLIF(main.census_tract, ""), "NA") AS census_tract,
+        IFNULL(NULLIF(main.exterior_condition, ""), "NA") AS exterior_condition,
+        IFNULL(NULLIF(main.zip_code, ""), "NA") AS zip_code,
+        IFNULL(NULLIF(main.zoning, ""), "NA") AS zoning,
+        IFNULL(NULLIF(main.number_of_bathrooms, ""), "NA") AS number_of_bathrooms,
+        IFNULL(NULLIF(main.number_of_bedrooms, ""), "NA") AS number_of_bedrooms,
+        IFNULL(NULLIF(main.number_stories, ""), "NA") AS number_stories,
+        IFNULL(NULLIF(main.total_area, ""), "NA") AS total_area,
+        IFNULL(NULLIF(main.year_built, ""), "NA") AS year_built
     FROM (
         SELECT *,
                ROW_NUMBER() OVER (
@@ -22,6 +23,14 @@ CREATE OR REPLACE TABLE `musa5090s25-team1.derived.current_assessments_model_tra
         WHERE sale_price IS NOT NULL 
           AND sale_price != ""
           AND SAFE_CAST(sale_price AS NUMERIC) > 1000
-    )
-    WHERE row_num = 1
+    ) AS main
+    LEFT JOIN (
+        SELECT sale_price, sale_date
+        FROM `musa5090s25-team1.core.opa_properties`
+        WHERE sale_price IS NOT NULL AND sale_date IS NOT NULL
+        GROUP BY sale_price, sale_date
+        HAVING COUNT(*) > 1
+    ) AS dup
+    ON main.sale_price = dup.sale_price AND main.sale_date = dup.sale_date
+    WHERE main.row_num = 1 AND dup.sale_price IS NULL
 );
