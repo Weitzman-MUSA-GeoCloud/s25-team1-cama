fffe91c35000534b221009bb122aa8f9bccc128e
BR <hbairun@gmail.com>
2025-04-16 15:44:37 -0400

    change to log

--- tasks/11_ml_model/sql/ml_model.sql
+++ tasks/11_ml_model/sql/ml_model.sql
@@ -1,18 +1,18 @@
-CREATE OR REPLACE MODEL `musa5090s25-team1.derived.sale_price_linear_regression`
+CREATE OR REPLACE MODEL `musa5090s25-team1.derived.sale_price_log_model`
 OPTIONS(
   model_type = 'linear_reg',
-  input_label_cols = ['sale_price']
+  input_label_cols = ['log_sale_price']
 ) AS
 
 SELECT
-  CAST(sale_price AS NUMERIC) AS sale_price,
-  census_tract,
+  LOG(CAST(sale_price AS NUMERIC)) AS log_sale_price,
   zip_code,
   CAST(number_of_bathrooms AS NUMERIC) AS number_of_bathrooms,
-  CAST(number_of_bedrooms AS NUMERIC) AS number_of_bedrooms,
+  CAST(number_of_bedrooms AS NUMERIC) AS number_of_bedrooms
 FROM
   `musa5090s25-team1.derived.current_assessments_model_training_data`
 WHERE
   SAFE_CAST(number_of_bathrooms AS NUMERIC) IS NOT NULL AND
   SAFE_CAST(number_of_bedrooms AS NUMERIC) IS NOT NULL AND
-  sale_price IS NOT NULL;
+  SAFE_CAST(sale_price AS NUMERIC) IS NOT NULL AND
+  CAST(sale_price AS NUMERIC) > 0;
