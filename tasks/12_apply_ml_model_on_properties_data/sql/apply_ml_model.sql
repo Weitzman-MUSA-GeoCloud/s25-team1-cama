e6a569c5663553ec2f18f3389c7c492908bf0d42
BR <hbairun@gmail.com>
2025-04-16 15:50:05 -0400

    fix log

--- tasks/12_apply_ml_model_on_properties_data/sql/apply_ml_model.sql
+++ tasks/12_apply_ml_model_on_properties_data/sql/apply_ml_model.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE TABLE `musa5090s25-team1.derived.current_assessments` AS
 SELECT
   property_id,
-  predicted_sale_price AS predicted_value,
+  EXP(predicted_log_sale_price) AS predicted_value,
   CURRENT_TIMESTAMP() AS predicted_at
 FROM ML.PREDICT(
-  MODEL `musa5090s25-team1.derived.sale_price_linear_regression`,
+  MODEL `musa5090s25-team1.derived.sale_price_log_model`,
   (
     SELECT
       property_id,
