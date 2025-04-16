247d1ed66761afe1a99ff4d94dbb4d36389e08c9
BR <hbairun@gmail.com>
2025-04-16 15:33:30 -0400

    initial code for applying model

---
+++ tasks/12_apply_ml_model_on_properties_data/issue12-terminal-code.sh
@@ -0,0 +1,13 @@
+gcloud functions deploy ml_model_apply \
+--region=us-east4 \
+--runtime=python312 \
+--source=. \
+--entry-point=run_sql \
+--service-account='data-pipeline-user@musa5090s25-team1.iam.gserviceaccount.com' \
+--timeout=999s \
+--memory=16Gi \
+--no-allow-unauthenticated \
+--trigger-http \
+--gen2
+
+gcloud functions call ml_model_apply --region=us-east4 --gen2
---
+++ tasks/12_apply_ml_model_on_properties_data/sql/apply_ml_model.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE TABLE `musa5090s25-team1.derived.current_assessments` AS
+SELECT
+  property_id,
+  predicted_sale_price AS predicted_value,
+  CURRENT_TIMESTAMP() AS predicted_at
+FROM ML.PREDICT(
+  MODEL `musa5090s25-team1.derived.sale_price_linear_regression`,
+  (
+    SELECT
+      property_id,
+      census_tract,
+      zip_code,
+      CAST(number_of_bathrooms AS NUMERIC) AS number_of_bathrooms,
+      CAST(number_of_bedrooms AS NUMERIC) AS number_of_bedrooms
+    FROM `musa5090s25-team1.core.opa_properties`
+    WHERE
+      SAFE_CAST(number_of_bathrooms AS NUMERIC) IS NOT NULL AND
+      SAFE_CAST(number_of_bedrooms AS NUMERIC) IS NOT NULL
+  )
+);
