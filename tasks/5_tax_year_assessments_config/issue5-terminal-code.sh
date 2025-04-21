bb90828639532fdecac3c54868c98f925e14b993
Akira Di Sandro <46532395+disandroa@users.noreply.github.com>
2025-04-21 13:35:08 -0400

    fixed issue 5 code to actually work with current assessment data
    Previously, the code for issue 5 erroneously called on previous tax year assessment data to make the current assessment bin JSON file. That error has been rememdied here

--- tasks/5_tax_year_assessments_config/issue5-terminal-code.sh
+++ tasks/5_tax_year_assessments_config/issue5-terminal-code.sh
@@ -1,14 +1,14 @@
 # check if function works locally
 functions-framework --debug \
-    --target generate_assessment_chart_configs
+    --target generate_current_assessment_chart_configs
  # --target generate-assessment-chart-configs
  # --target generate_assessment_chart_configs
 
-gcloud functions deploy generate-assessment-chart-configs \
+gcloud functions deploy generate-current-assessment-chart-configs \
 --region=us-east4 \
 --runtime=python312 \
 --source=. \
---entry-point=generate_assessment_chart_configs \
+--entry-point=generate_current_assessment_chart_configs \
 --service-account='data-pipeline-user@musa5090s25-team1.iam.gserviceaccount.com' \
 --timeout=999s \
 --memory=8Gi \
@@ -16,4 +16,4 @@ gcloud functions deploy generate-assessment-chart-configs \
 --set-env-vars='DATA_LAKE_PUBLIC=musa5090s25-team1-public' \
 --trigger-http
 
-gcloud functions call generate-assessment-chart-configs --region=us-east4 --gen2
+gcloud functions call generate-current-assessment-chart-configs --region=us-east4 --gen2
