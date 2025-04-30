a0e638527c941bcedad55e2d847b9c0f527a5c50
Akira Di Sandro <46532395+disandroa@users.noreply.github.com>
2025-04-30 13:55:21 -0400

    completed issue 20, but it will probably break, so don't approve this request yet hehe

--- tasks/20_map_styling_metadata_JSON/main.py
+++ tasks/20_map_styling_metadata_JSON/main.py
@@ -10,152 +10,26 @@ dotenv.load_dotenv()
 def create_mapstyle_metadata(request):
     bigquery_client = bigquery.Client()
 
-    # Current Assessed Values
-    print('Starting current assessment query...')
-    sql = '''
-        SELECT
-            APPROX_QUANTILES(predicted_value, 5) AS thisyear_quin
-        FROM `musa5090s25-team1.derived.current_assessments`
-    '''
+    sql_file = './styling_metadata.sql'
 
-    query_results = bigquery_client.query_and_wait(sql)
-    # rows = list(query_results)
-    print('Finished current assessment query.')
+    sql_query = sql_file.read()
+
+    print('Starting query...')
+    query_results = bigquery_client.query_and_wait(sql_query)
+    rows = list(query_results)
+    print('Finished query, hooray!')
 
     features = []
-    features.append({
-        'type': 'Feature',
-        'current_assessed_value': {
-            'min': query_results[0],
-            'max': query_results[-1],
-            'breakpoints': query_results
-        }
-    })
-    print('Added current assessment data to feature collection.')
-
-
-    # Last year's Assessed Values
-    print('Starting previous assessment query...')
-    sql = '''
-        SELECT
-            APPROX_QUANTILES(PARSE_NUMERIC(market_value), 5) AS lastyear_quin
-        FROM `musa5090s25-team1.source.opa_assessments`
-        WHERE year = '2024' AND market_value > 1000
-    '''
-
-    query_results = bigquery_client.query_and_wait(sql)
-    # rows = list(query_results)
-    print('Finished previous assessment query.')
-
-    features.append({
-        'type': 'Feature',
-        'tax_year_assessed_value': {
-            'min': query_results[0],
-            'max': query_results[-1],
-            'breakpoints': query_results
-        }
-    })
-    print('Added previous assessment data to feature collection.')
-
-
-    # Absolute Difference (between last year's and current assessed values)
-    print('Starting absolute difference query...')
-    sql = '''
-        WITH lastyear AS (
-            SELECT
-                parcel_number,
-                PARSE_NUMERIC(market_value) AS market_value
-            FROM `musa5090s25-team1.source.opa_assessments`
-            WHERE year = '2024'
-        ),
-
-        thisyear AS (
-            SELECT
-                property_id,
-                predicted_value
-            FROM `musa5090s25-team1.derived.current_assessments`
-        ),
-
-        bothyears AS (
-          SELECT
-              ly.parcel_number,
-              ly.market_value,
-              ty.predicted_value
-              ROUND(ty.predicted_value - ly.market_value) AS dif,
-              ROUND(100*(ty.predicted_value - ly.market_value)/ly.market_value, 2) AS perc_dif
-          FROM thisyear AS ty
-          JOIN lastyear AS ly 
-              ON ty.property_id = ly.parcel_number
-        )
-
-        SELECT
-            APPROX_QUANTILES(ABS(dif), 3) AS absdif_quin
-        FROM bothyears
-    '''
-
-    query_results = bigquery_client.query_and_wait(sql)
-    # rows = list(query_results)
-    print('Finished absolute difference query.')
-
-    features.append({
-        'type': 'Feature',
-        'tax_year_assessed_value': {
-            'min': query_results[0],
-            'max': query_results[-1],
-            'breakpoints': query_results
-        }
-    })
-    print('Added absolute difference data to feature collection.')
-
-
-    # Percent Difference (between last year's and current assessed values)
-    print('Starting percent difference query...')
-    sql = '''
-        WITH lastyear AS (
-            SELECT
-                parcel_number,
-                PARSE_NUMERIC(market_value) AS market_value
-            FROM `musa5090s25-team1.source.opa_assessments`
-            WHERE year = '2024'
-        ),
-
-        thisyear AS (
-            SELECT
-                property_id,
-                predicted_value
-            FROM `musa5090s25-team1.derived.current_assessments`
-        ),
-
-        bothyears AS (
-          SELECT
-              ly.parcel_number,
-              ly.market_value,
-              ty.predicted_value
-              ROUND(ty.predicted_value - ly.market_value) AS dif,
-              ROUND(100*(ty.predicted_value - ly.market_value)/ly.market_value, 2) AS perc_dif
-          FROM thisyear AS ty
-          JOIN lastyear AS ly 
-              ON ty.property_id = ly.parcel_number
-        )
-
-        SELECT
-            APPROX_QUANTILES(ABS(perc_dif), 3) AS percdif_quin
-        FROM bothyears
-    '''
-
-    query_results = bigquery_client.query_and_wait(sql)
-    # rows = list(query_results)
-    print('Finished percent difference query.')
-
-    features.append({
-        'type': 'Feature',
-        'tax_year_assessed_value': {
-            'min': -query_results[-1],
-            'max': query_results[-1],
-            'breakpoints': list(-query_results[-1], -query_results[-2], query_results[-3], query_results)
-        }
-    })
-    print('Added percent difference data to feature collection.')
+    for row in rows:
+        features.append({
+            'type': 'Feature',
+            'properties': {
+                'field': row['styling_metadata. field'],
+                'min_value': row['styling_metadata. min_value'],
+                'max_value': row['styling_metadata. max_value'],
+                'quintiles': row['styling_metadata. quintiles']
+            }
+        })
 
     feature_collection = {
         'type': 'FeatureCollection',
---
+++ tasks/20_map_styling_metadata_JSON/styling_metadata.sql
@@ -0,0 +1,97 @@
+WITH lastyear AS (
+    SELECT
+        parcel_number,
+        PARSE_NUMERIC(market_value) AS market_value
+    FROM `musa5090s25-team1.source.opa_assessments`
+    WHERE year = '2024'
+),
+
+thisyear AS (
+    SELECT
+        property_id,
+        predicted_value
+    FROM `musa5090s25-team1.derived.current_assessments`
+),
+
+bothyears AS (
+    SELECT
+        ly.parcel_number,
+        ly.market_value,
+        ty.predicted_value,
+        CASE ly.market_value
+            WHEN 0 THEN NULL
+            ELSE ROUND(ty.predicted_value - ly.market_value, 2)
+        END AS dif,
+        CASE ly.market_value
+            WHEN 0 THEN NULL
+            ELSE ROUND(100 * (ty.predicted_value - ly.market_value) / ly.market_value, 2)
+        END AS perc_dif
+    FROM thisyear AS ty
+    JOIN lastyear AS ly
+        ON ty.property_id = ly.parcel_number
+),
+
+market_value_stats AS (
+    SELECT
+        'market_value' AS field,
+        MIN(ly.market_value) AS min_value,
+        MAX(ly.market_value) AS max_value,
+        APPROX_QUANTILES(ly.market_value, 5) AS quintiles
+    FROM lastyear AS ly
+    WHERE ly.market_value > 1000
+),
+
+predicted_value_stats AS (
+    SELECT
+        'predicted_value' AS field,
+        MIN(ty.predicted_value) AS min_value,
+        MAX(ty.predicted_value) AS max_value,
+        APPROX_QUANTILES(ty.predicted_value, 5) AS quintiles
+    FROM thisyear AS ty
+),
+
+absolute_difference_stats AS (
+    SELECT
+        'absolute_difference' AS field,
+        MIN(dif) AS min_value,
+        MAX(dif) AS max_value,
+        ARRAY_CONCAT(
+            [MIN(dif)],
+            [-2 * STDDEV(dif)],
+            [-STDDEV(dif)],
+            [STDDEV(dif)],
+            [2 * STDDEV(dif)],
+            [MAX(dif)]
+        ) AS quintiles
+    FROM bothyears
+),
+
+percent_difference_stats AS (
+    SELECT
+        'percent_difference' AS field,
+        MIN(perc_dif) AS min_value,
+        MAX(perc_dif) AS max_value,
+        ARRAY_CONCAT(
+            [MIN(perc_dif)],
+            [-65.0],
+            [-20.0],
+            [20.0],
+            [65.0],
+            [MAX(perc_dif)]
+        ) AS quintiles
+    FROM bothyears
+),
+
+stats AS (
+    SELECT field, min_value, max_value, quintiles FROM market_value_stats
+    UNION ALL
+    SELECT field, min_value, max_value, ARRAY(SELECT CAST(value AS NUMERIC) FROM UNNEST(quintiles) AS value) FROM predicted_value_stats
+    UNION ALL
+    SELECT field, min_value, max_value, ARRAY(SELECT CAST(value AS NUMERIC) FROM UNNEST(quintiles) AS value) FROM absolute_difference_stats
+    UNION ALL
+    SELECT field, min_value, max_value, ARRAY(SELECT CAST(value AS NUMERIC) FROM UNNEST(quintiles) AS value) FROM percent_difference_stats
+)
+
+SELECT
+    ARRAY_AGG(STRUCT(field, min_value, max_value, quintiles)) AS styling_metadata
+FROM stats;
